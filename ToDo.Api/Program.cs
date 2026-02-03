d3b25a6949b4272ae209b75b95bcce14e7d76f86
Vaidehi Hirani <vaidehi.ketan.oct2004@gmail.com>
2026-02-03 11:59:16 +0530

    swagger open automatically on dotnet run command

--- ToDo.Api/Program.cs
+++ ToDo.Api/Program.cs
@@ -74,4 +74,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+if (app.Environment.IsDevelopment())
+{
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path == "/")
+        {
+            context.Response.Redirect("/swagger");
+            return;
+        }
+        await next();
+    });
+}
+
+
 app.Run();
